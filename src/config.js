972b93531ddf40d8ca6b221fe24b88082af12bb5
Jordao Frazao <jordaofrazao@gmail.com>
2017-02-13 19:14:16 +0000

    add environment variables

--- src/config.js
+++ src/config.js
@@ -6,10 +6,11 @@
  */
 require('dotenv').config()
 
-module.exports = {
+/**module.exports = {
   consumer_key: "5gpKS3tdvtydZOq8cOTxMbxPF",
   consumer_secret: "ykAyYLNMpzWxcDIO4UPmItUE0ix0jmIA0elnaur2s0Qt7uk5VX",
   access_token: "829311315042791424-wtely5Xma51yDPG6mN9LdnAsee3XlVD",
   access_token_secret: "uPEhD9QtoaJrIeMiUyyCyNqH7xoy8eRsODWGlwzyMrjz2",
   sentiment_key: "mWWQq3FEvFmshAJtzCRtenOsiOx8p1jM1qnjsn6ipImPMp8Zgr"
 }
+*/
